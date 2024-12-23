2725d6e994e33e3df45f34c2d9cc80b37c9f4505
DanilodeRamos <Danilodramos@proton.me>
2024-12-23 10:16:03 -0300

    exercicio exceçoes try catch

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,87 @@
 package application;
+/*Problema exemplo
+Fazer um programa para ler os dados de uma reserva de hotel(número do quarto,data
+de entrada e data de saída)e mostrar os dados da reserva, inclusive sua duração em
+dias. Em seguida, ler novas datas de entrada e saída, atualizar a reserva, e mostrar
+novamente a reserva comos dados atualizados.O programa não deve aceitar dados
+inválidos para a reserva,conforme as seguintes regras:-Alterações de reserva só 
+podem ocorrer para datas futuras-A data de saída deve ser maior que a data de entrada
+ Examples1
+ Roomnumber: 8021
+ Check-in date (dd/MM/yyyy): 23/09/2019
+ Check-out date (dd/MM/yyyy): 26/09/2019
+ Reservation: Room 8021, check-in: 23/09/2019, check-out: 26/09/2019, 3 nights
+ Enter data to update the reservation:
+ Check-in date (dd/MM/yyyy): 24/09/2019
+ Check-out date (dd/MM/yyyy): 29/09/2019
+ Reservation: Room 8021, check-in: 24/09/2019, check-out: 29/09/2019, 5 nights
+ Roomnumber: 8021
+ Check-in date (dd/MM/yyyy): 23/09/2019
+ Check-out date (dd/MM/yyyy): 21/09/2019
+ Errorin reservation: Check-out date must beaftercheck-in date
+ 
+  Examples2
+ Roomnumber: 8021
+ Check-in date (dd/MM/yyyy): 23/09/2019
+ Check-out date (dd/MM/yyyy): 26/09/2019
+ Reservation: Room 8021, check-in: 23/09/2019, check-out: 26/09/2019, 3 nights
+ Enter data to update the reservation:
+ Check-in date (dd/MM/yyyy): 24/09/2015
+ Check-out date (dd/MM/yyyy): 29/09/2015
+ Errorin reservation: Reservation dates for updatemust be future dates
+ Roomnumber: 8021
+ Check-in date (dd/MM/yyyy): 23/09/2019
+ Check-out date (dd/MM/yyyy): 26/09/2019
+ Reservation: Room 8021, check-in: 23/09/2019, check-out: 26/09/2019, 3 nights
+ Enter data to update the reservation:
+ Check-in date (dd/MM/yyyy): 24/09/2020
+ Check-out date (dd/MM/yyyy): 22/09/2020
+ Errorin reservation: Check-out date must beaftercheck-in date
+*/
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+			
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+			
+			System.out.println();
+			System.out.println("Enter data to update the reservation:");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+		}
+		catch (ParseException e) {
+			System.out.println("Invalid date format");
+		}
+		catch (DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
 
+		sc.close();
 	}
-
 }
